aad5fb118bb3412a404ddf974519806a9fdc262d
lingPoint <44696270+lingPoint@users.noreply.github.com>
2023-02-03 13:33:02 +0800

    Update blogkit.config.ts

--- blogkit.config.ts
+++ blogkit.config.ts
@@ -9,5 +9,25 @@ export default defineConfig({
     url: "https://my-blog-delta-sooty.vercel.app",
   },
   theme,
+  themeConfig: {
+    links: [
+      {
+        name: "GitHub",
+        url: "https://github.com/lingPoint",
+      },
+      //{
+      //  name: "Twitter",
+      //  url: "https://twitter.com/_2nthony",
+      // },
+      {
+        name: "RSS",
+        url: "/api/rss",
+      },
+    ],
+    introduceHTML: `
+      <p>我是一名普通的程序员，擅长 web 前端开发，现生活在广州。</p>
+      <p>我的目标是真正的成为一名自由职业者，拥有一些小产品，并进入 FIRE(Financial Independence Retire Early) 人生。</p>
+    `,
+  },
   request,
 });
