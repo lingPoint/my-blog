6b26681396ba49cd3f90761510db5cf78352374a
lingPoint <44696270+lingPoint@users.noreply.github.com>
2023-02-05 11:40:47 +0800

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,3 +3,9 @@ import { blogkit } from "./_app";
 export default blogkit.config.theme.Home;
 
 export const getStaticProps = blogkit.getHomePageStaticProps;
+
+function HomePage() {
+  return <div>Welcome to Next.js!</div>
+};
+
+export default HomePage;
