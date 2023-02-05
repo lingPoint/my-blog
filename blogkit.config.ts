07fac555f17edee1b7055c49963e3bba8b2c08ab
lingPoint <44696270+lingPoint@users.noreply.github.com>
2023-02-05 18:23:12 +0800

    Update blogkit.config.ts

--- blogkit.config.ts
+++ blogkit.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
     author: "Zerox",
     url: "https://zerox.cc/",
   },
-  theme: {},
+  theme,
   themeConfig: {
     links: [
       {
@@ -30,12 +30,12 @@ export default defineConfig({
   	<a href="#" id="hitokoto_text"></a>
 	</p>
 	<script src="https://v1.hitokoto.cn/?encode=js&select=%23hitokoto" defer></script>
-      <script type="text/javascript">
+      	<script type="text/javascript">
 //       window.onload = function(){
-// 	    //document.getElementsByClassName("my-16")[0].style.display="none";
-// 	    document.getElementsByClassName("my-16")[0].innerHTML = '<p>Powered by Zerox</p>';
+// 	 //document.getElementsByClassName("my-16")[0].style.display="none";
+// 	 document.getElementsByClassName("my-16")[0].innerHTML = '<p>Powered by Zerox</p>';
 //       }
-       </script>
+        </script>
     `,
   },
   request,
