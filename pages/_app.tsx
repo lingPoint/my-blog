1992764825fda0c2baa57a2ef0975f0ce1f78ddf
lingPoint <44696270+lingPoint@users.noreply.github.com>
2023-02-05 11:28:45 +0800

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,7 @@ import type { AppProps } from "next/app";
 import { Blogkit } from "blogkit";
 import config from "../blogkit.config";
 
-import "../blogkit-theme-minimal/style.css";
+import "blogkit-theme-minimal/style.css";
 
 export const blogkit = new Blogkit(config);
 
