5b8a447e5473146d3ea1749cdff767b0270ce8d2
lingPoint <44696270+lingPoint@users.noreply.github.com>
2023-02-05 20:58:57 +0800

    Update blogkit.config.ts

--- blogkit.config.ts
+++ blogkit.config.ts
@@ -11,34 +11,28 @@ export default defineConfig({
     url: "https://zerox.cc/",
   },
   theme,
-//   themeConfig: {
-//     links: [
-//       {
-//         name: "GitHub",
-//         url: "https://github.com/lingPoint",
-//       },
-//       //{
-//       //  name: "Twitter",
-//       //  url: "https://my-blog-delta-sooty.vercel.app",
-//       // },
-//       {
-//         name: "RSS",
-//         url: "/api/rss",
-//       },
-//     ],
-//     introduceHTML: `
-//       <p>对.这是一个博客</p>
-//       <p id="hitokoto">
-//   	<a href="#" id="hitokoto_text"></a>
-// 	</p>
-// 	<script src="https://v1.hitokoto.cn/?encode=js&select=%23hitokoto" defer></script>
-//       	<script type="text/javascript">
-// //       window.onload = function(){
-// // 	 //document.getElementsByClassName("my-16")[0].style.display="none";
-// // 	 document.getElementsByClassName("my-16")[0].innerHTML = '<p>Powered by Zerox</p>';
-// //       }
-//         </script>
-//     `,
-//   },
+  themeConfig: {
+    links: [
+      {
+        name: "GitHub",
+        url: "https://github.com/lingPoint",
+      },
+      //{
+      //  name: "Twitter",
+      //  url: "https://my-blog-delta-sooty.vercel.app",
+      // },
+      {
+        name: "RSS",
+        url: "/api/rss",
+      },
+    ],
+    introduceHTML: `
+      <p>对.这是一个博客</p>
+      <p id="hitokoto">
+  	  <a href="#" id="hitokoto_text"></a>
+	    </p>
+	    <script src="https://v1.hitokoto.cn/?encode=js&select=%23hitokoto" defer></script>
+    `,
+  },
   request,
 });
