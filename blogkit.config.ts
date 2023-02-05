df368619821ab3ce0b406038c2bccd2b5c0dd629
lingPoint <44696270+lingPoint@users.noreply.github.com>
2023-02-05 20:59:28 +0800

    Update blogkit.config.ts

--- blogkit.config.ts
+++ blogkit.config.ts
@@ -29,9 +29,9 @@ export default defineConfig({
     introduceHTML: `
       <p>对.这是一个博客</p>
       <p id="hitokoto">
-  	  <a href="#" id="hitokoto_text"></a>
-	    </p>
-	    <script src="https://v1.hitokoto.cn/?encode=js&select=%23hitokoto" defer></script>
+      <a href="#" id="hitokoto_text"></a>
+      </p>
+      <script src="https://v1.hitokoto.cn/?encode=js&select=%23hitokoto" defer></script>
     `,
   },
   request,
