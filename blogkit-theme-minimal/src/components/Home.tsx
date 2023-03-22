8821ddd5270cc8e281d056e47d2d12ce9fa301dd
Zero <44696270+lingPoint@users.noreply.github.com>
2023-03-22 12:45:24 +0800

    Update Home.tsx

--- blogkit-theme-minimal/src/components/Home.tsx
+++ blogkit-theme-minimal/src/components/Home.tsx
@@ -14,7 +14,7 @@ export function Home({ posts, siteConfig, themeConfig }: HomePageProps) {
       </Head>
 
       <div className="flex items-center">
-  <img src="https://avatars.githubusercontent.com/u/44696270?s=40&v=4" alt="Logo" className="w-10 h-10 mr-2" />
+  <img href="https://avatars.githubusercontent.com/u/44696270?s=40&v=4" alt="Logo" className="w-10 h-10 mr-2" />
   <h1 className="text-4xl font-extrabold">{siteConfig.name}</h1>
 </div>
 
